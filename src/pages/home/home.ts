9306e7e48cfdb47f664d6ae92368e0f4bac8b8a2
Will Xavier <willyelnsg.x@gmail.com>
2018-06-30 22:58:33 -0300

    Issue #2: Create page and initial interface elements: - User Info - Home List Money Logout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MenuPage } from '../menu/menu';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  toMenu() {
+    this.navCtrl.push(MenuPage);
   }
 
 }
